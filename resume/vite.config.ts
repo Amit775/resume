9122fb4704ca9510250a0d04d6cdde1341bb8389
Amit775 <38419204+Amit775@users.noreply.github.com>
2024-06-14 18:22:59 +0300

    add build script

--- resume/vite.config.ts
+++ resume/vite.config.ts
@@ -1,46 +1,56 @@
 /// <reference types="vitest" />
 
-import analog from '@analogjs/platform';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import analog from "@analogjs/platform";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     root: __dirname,
-    publicDir: 'src/public',
+    publicDir: "src/public",
     cacheDir: `../node_modules/.vite`,
-    
+
     build: {
-      outDir: '../dist/./resume/client',
-      reportCompressedSize: true,    
-      target: ['es2020'],
+      outDir: "../dist/./resume/client",
+      reportCompressedSize: true,
+      target: ["es2020"],
     },
     server: {
       fs: {
-        allow: ['.'],
+        allow: ["."],
       },
-    },    
+    },
     plugins: [
-      
       analog({
-		vite: {
-			inlineStylesExtension: 'scss'
-		}
-	  }),
-      
+        vite: {
+          inlineStylesExtension: "scss",
+        },
+        nitro: {
+          preset: "firebase",
+          firebase: {
+            nodeVersion: "20",
+            gen: 2,
+            httpsOptions: {
+              region: "europe-west1",
+              maxInstances: 100,
+            },
+          },
+        },
+      }),
+
       nxViteTsPaths(),
       splitVendorChunkPlugin(),
     ],
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: ['src/test-setup.ts'],
-      include: ['**/*.spec.ts'],
-      reporters: ['default'],
+      environment: "jsdom",
+      setupFiles: ["src/test-setup.ts"],
+      include: ["**/*.spec.ts"],
+      reporters: ["default"],
     },
     define: {
-      'import.meta.vitest': mode !== 'production',
+      "import.meta.vitest": mode !== "production",
     },
   };
 });
